22982984948377e3ddf8710825c003c1a86a8361
Osvaldo Lamas <osvaldolamas@gmail.com>
2017-01-30 18:15:54 -0300

    Fixing test packages

--- src/test/java/com/galaxy/weather/GalaxyWeatherApplicationTests.java
+++ src/test/java/com/galaxy/weather/GalaxyWeatherApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.galaxy.weather;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class GalaxyWeatherApplicationTests {
 
 	@Test
 	public void contextLoads() {
